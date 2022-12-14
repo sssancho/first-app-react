1d69718346c4ba06326b6f59d7ccd7a1c4f6b9ed
sssancho <blogersanjar@gmail.com>
2022-12-14 14:56:35 +0100

    Styling differences IOS&Android

--- RNcourse/App.js
+++ RNcourse/App.js
@@ -30,7 +30,11 @@ export default function App() {
       </View>
 
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map((goal) => (
+          <View style={styles.goalItem}>
+            <Text style={styles.goalText} key={goal}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   )
@@ -63,6 +67,17 @@ const styles = StyleSheet.create({
 
   goalsContainer:{
     flex: 4,
+  },
+
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+
+  goalText:{
+    color: 'white'
   }
   
 });
