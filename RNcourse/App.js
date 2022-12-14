ad84e2d2277f4dd86080319627f013d117be97bc
sssancho <blogersanjar@gmail.com>
2022-12-14 14:24:00 +0100

    Add 2 function

--- RNcourse/App.js
+++ RNcourse/App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+  }
+
+  function addGoalHandler() {}
+
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your goals" />
+        <TextInput 
+          style={styles.textInput} 
+          placeholder="Your goals" 
+          onChangeText={goalInputHandler}
+        />
         <Button title="Add goal"/>
       </View>
 
@@ -13,7 +24,7 @@ export default function App() {
         <Text>List of Goals</Text>
       </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
